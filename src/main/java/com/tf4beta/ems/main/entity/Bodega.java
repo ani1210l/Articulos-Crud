4a9fb0a65336614bb219cbef772fd29f16c6d9db
franxxdmsk132 <tapiafranklin666@gmail.com>
2023-11-23 18:00:42 -0500

    hola gordo

--- src/main/java/com/tf4beta/ems/main/entity/Bodega.java
+++ src/main/java/com/tf4beta/ems/main/entity/Bodega.java
@@ -1,4 +1,18 @@
 package com.tf4beta.ems.main.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Bodega")
 public class Bodega {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "codigo_bodega")
+    private int codigo_bodega;
+
+    @Column()
+
+
 }
