52c22c8f160308601da93514567702cd0eddee48
ani1210l <122383018+ani1210l@users.noreply.github.com>
2023-11-28 00:55:34 -0500

    creacion de ingresos_cab,rowmapper

---
+++ src/main/java/com/tf4beta/ems/main/repository/IngresoRepository.java
@@ -0,0 +1,7 @@
+package com.tf4beta.ems.main.repository;
+
+import com.tf4beta.ems.main.entity.Ingreso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngresoRepository extends JpaRepository<Ingreso , Integer> {
+}
