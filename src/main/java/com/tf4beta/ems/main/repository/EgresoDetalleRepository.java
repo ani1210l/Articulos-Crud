c17f4d3931ce221b056cf2403c4a451110e17c8e
franxxdmsk132 <tapiafranklin666@gmail.com>
2023-11-28 00:54:46 -0500

    renombre

--- src/main/java/com/tf4beta/ems/main/repository/EgresoDetalleRepository.java
+++ src/main/java/com/tf4beta/ems/main/repository/EgresoDetalleRepository.java
@@ -1,4 +1,7 @@
 package com.tf4beta.ems.main.repository;
 
-public interface EgresoDetalleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.relational.core.sql.In;
+
+public interface EgresoDetalleRepository extends JpaRepository<EgresoDetalleRepository , Integer> {
 }
