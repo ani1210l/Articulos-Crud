a760aed3371965ff6a30c71e1cedd45c50477b3a
franxxdmsk132 <tapiafranklin666@gmail.com>
2023-11-28 23:24:46 -0500

    egreso vacio

--- src/main/java/com/tf4beta/ems/main/dao/EgresoDetallesDao.java
+++ src/main/java/com/tf4beta/ems/main/dao/EgresoDetallesDao.java
@@ -1,4 +1,10 @@
 package com.tf4beta.ems.main.dao;
 
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public class EgresoDetallesDao {
 }
--- src/main/java/com/tf4beta/ems/main/repository/EgresoDetalleRepository.java
+++ src/main/java/com/tf4beta/ems/main/repository/EgresoDetalleRepository.java
@@ -1,7 +1,8 @@
 package com.tf4beta.ems.main.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.relational.core.sql.In;
+import org.springframework.stereotype.Repository;
 
-public interface EgresoDetalleRepository extends JpaRepository<EgresoDetalleRepository , Integer> {
+@Repository
+public interface EgresoDetalleRepository{
 }
