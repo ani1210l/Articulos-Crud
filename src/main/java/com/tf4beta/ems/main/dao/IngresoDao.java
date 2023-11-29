e232d9429bb84137c5d876844dcdf07e4c24f2d4
ani1210l <122383018+ani1210l@users.noreply.github.com>
2023-11-29 10:24:21 -0500

    HOLA MAMOR DE POLLO

--- src/main/java/com/tf4beta/ems/main/dao/IngresoDao.java
+++ src/main/java/com/tf4beta/ems/main/dao/IngresoDao.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -18,13 +19,23 @@ public class IngresoDao {
 
 
     public void save (Ingreso ingreso){
-        String sql = "INSERT INTO ingreso (fecha, codigo_bodega) VALUES (?,?)":
-        JdbcTemplate.update
+        String sql = "INSERT INTO ingreso (fecha, codigo_bodega) VALUES (?, ?)";
+        jdbcTemplate.update
                 (sql,
                         ingreso.getFecha(),
                         ingreso.getBodega().getCodigo_bodega()
                 );
     }
+    public void update (Ingreso ingreso) {
+        String sql = "UPDATE ingreso SET fecha = ?, codigo_bodega =? WHERE ingreso.id_ingreso_cab = ? ";
+        jdbcTemplate.update
+                (sql,
+                        ingreso.getFecha(),
+                        ingreso.getBodega().getCodigo_bodega()
+                );
+    }
+
+
     public Ingreso findById(int id_ingresocab){
         String sql = "SELECT i.*, b.codigoB FROM ingreso i LEFT JOIN bodeg b ON a.codigo_bodega = b.codigo_bodega WHERE id_ingresocab = ?";
         return jdbcTemplate.queryForObject(sql, new IngresoRowMapper(), id_ingresocab);
@@ -34,8 +45,18 @@ public class IngresoDao {
 return jdbcTemplate.queryForObject(sql, new IngresoRowMapper(),id_ingresocab);
 
     }
-    ///public List<Ingreso>findAll(){
-        ////String sql = "SELECT * FROM ingreso LEFT JOIN bodega ON ingreso.codigo_bodega= bodega.codigoBodega";
-        ////return jdbcTemplate.queryForObject(sql.new IngresoRowMapper());
+    public List<Ingreso> findAll() {
+        String sql = "SELECT * FROM ingreso LEFT JOIN bodega ON ingreso.codigo_bodega= bodega.codigoBodega";
+        return jdbcTemplate.query(sql, new IngresoRowMapper());
+
+    }
+    public List <Ingreso> findAllWhithBodegaDetaills(){
+        String sql = "SELECT i.*, b.codigo_bodega, b.nombre , b.codigob FROM ingreso i " +
+                "LEFT JOIN bodega b ON i.codigo_bodega = b.codigo_bodega";
+        return jdbcTemplate.query(sql, new IngresoRowMapper());
+    }
+    public void delate(Integer id_ingresocab){
+        String sql = "DELATE FROM ingreso WHERE id_ingreso_cab = '"+ id_ingresocab + "'";
+        jdbcTemplate.update(sql);
     }
 }
